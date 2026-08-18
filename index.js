938dd55ff001a26bd5b0810a4d1b1109c8cdda35
Sudha.... <sudharanibisai2007@gmail.com>
2026-08-18 22:52:06 +0530

    Rename db variable to connection in index.js

--- index.js
+++ index.js
@@ -12,12 +12,12 @@ app.use(methodOverride("_method"));
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
-const db = mysql.createConnection({
+const connection = mysql.createConnection({
     host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT
 });
 app.get("/user", (req, res) => {
     const q = "SELECT * FROM student_info";
